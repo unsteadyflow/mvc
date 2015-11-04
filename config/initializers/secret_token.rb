2a9b9ff069db1f32ed035fc9409f1ce27b5f18f7
unsteadyflow <du021000@gmail.com>
2015-11-04 10:07:08 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mvc::Application.config.secret_key_base = '31badd40d05a1a853f8c02f474509717e4450e0af7e35133bf6ab9f8035e036fe0b541fc82848493efe6a14e980ac179564bc5418c608d0c830827b1d11b72c5'
